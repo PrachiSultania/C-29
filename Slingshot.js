b6b7d5e92f962d2648762908b0f81ea81e01d7a7
unknown <prachisultania007@gmail.com>
2021-04-03 16:39:51 +0530

    grand catapult ready

--- Slingshot.js
+++ Slingshot.js
@@ -8,7 +8,11 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
+
     }
 
     fly(){
@@ -16,12 +20,30 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 180, 20);
+        image(this.sling2, 150, 15);
+        
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+            stroke("#301608")
+            if(pointA.x<220){
+                strokeWeight(10)
+                line(pointA.x-25, pointA.y, pointB.x-30, pointB.y+20)
+                line(pointA.x-25, pointA.y, pointB.x+15, pointB.y+20)
+                image(this.sling3, pointA.x -30, pointA.y - 10, 15, 30 );
+            }
+            else{
+                strokeWeight(5)
+                line(pointA.x+25, pointA.y, pointB.x-30, pointB.y+20)
+                line(pointA.x+25, pointA.y, pointB.x+15, pointB.y+20)
+                image(this.sling3, pointA.x +25, pointA.y - 10, 15, 30 );  
+            }
         }
+
+
     }
     
 }
